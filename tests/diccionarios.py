0f2feff08085f935af07d242fad7866af12082a3
G4ll4rd0 <43729882+G4ll4rd0@users.noreply.github.com>
2021-04-01 18:20:24 -0600

    Se agregan los comentarios de los codigos de futureDicctionary. py y doubleSplitting.py

--- tests/diccionarios.py
+++ tests/diccionarios.py
@@ -3,9 +3,11 @@ import operacionesBasicas.operaciones.multiplicacion
 import operacionesBasicas.operaciones.suma
 import operacionesBasicas.operaciones.resta
 
+#Se solicitan dos numeros
 x1 = int(input("x1 = "))
 x2 = int(input("x2 = "))
 
+#Una lista bidfimensional que posee la operacion que se debe mandar a llamar, el nombre de la operacion y la sintaxis con la que se le ingresa
 operaciones = [
     ["\div", "división", operacionesBasicas.operaciones.division.division(x1,x2)],
     ["\mult", "multiplicación", operacionesBasicas.operaciones.multiplicacion.multiplicacion(x1,x2)],
@@ -15,6 +17,7 @@ operaciones = [
 
 opcion = input("Que desea Realizar? ")
 
+#Iteramos el arreglo 2D hasta dar con la opcion que cuadre con lo que hemos ingresado, una vez ahi imprimeremos el nombre de la funcion y el resultado de la misma
 for x,i,j in operaciones:
     if (x) == opcion:
         print(i,j)
--- tests/futureDicctionary.py
+++ tests/futureDicctionary.py
@@ -4,7 +4,9 @@ import operacionesBasicas.operaciones.suma
 import operacionesBasicas.operaciones.resta
 import asyncio
 
+#Funcion para establecer el resultado de la operacion que deseamos
 async def setAfter(r, x1, x2, opc):
+    #Arreglo bidimensional con el nombre comun de la operacion y la funcion que la realiza
     operaciones = [
         ["división", operacionesBasicas.operaciones.division.division( x1,  x2)],
         ["multiplicación", operacionesBasicas.operaciones.multiplicacion.multiplicacion( x1,  x2)],
@@ -12,22 +14,31 @@ async def setAfter(r, x1, x2, opc):
         ["resta", operacionesBasicas.operaciones.resta.resta( x1,  x2)]
     ]
 
+    #Ciclo para obtener el resultado de la operacion que deseamos
     for i,j in operaciones:
         if i == opc:
             n = j
 
+    #Se fija el resultado de lo que hemos solicitado
     r.set_result(n)
 
+#Funcion Principal
 async def main():
+    #Se le solicita al usario las operaciones que desea realizar y en caso de que sea mas de una se separa en una lista que luego usaremos para ir resolviendo
+    #TODO: FALTA AGREGAR SOPORTE PARA PARENTESIS Y O JERARQUIA DE OPERACIONES, AHORITA HACE TODO LINEAL
+    #?Se esta trabajando con un ejemplo de parentesis en el archivo de doubleSplitting.py
     opcion = input("Que desea Realizar? ")
 
     opc = opcion.split("\\")
     opc.remove("")
     
+    #Asignamos un contador para saber si tendremos que pedir mas de un valor 
     contador = 0
 
+    #Se crea un looppara nuestra funcion asincronica
     loop = asyncio.get_running_loop()
 
+    #Lista bidimensional con los nombres ocmunes de las operaciones y la sintaxis con la que queda separada
     operaciones = [
         ["div", "división"],
         ["mult", "multiplicación"],
@@ -35,16 +46,25 @@ async def main():
         ["minus", "resta"]
     ]
 
+    #Se itera primero la lista de operaciones que quiere realizar el usuario
     for h in opc:
+        #Dentro de esta iteramos nuesro diccionario de operaciones
         for x,i in operaciones:
+            #Si el item de la lista de operaciones a realizar concuerda con el item de la lista de operaciones posibles, se manda obtener ese resultado
             if x == h:
+                #Solo la primera vez que se ejecute deberemos de solicitar el primer numeo, luego solo sera el segundo ya que el resultado anterior fungira como el primero
                 if contador == 0:
                     x1 = int(input("x1: "))
                 x2 = int(input("x2: "))
+                #Creamos nuestro objeto que sera completado en un futuro
                 r = loop.create_future()
+                #Mandamos obtener el resultado con esta funcion, pasandole el nombre de la operacion a realizar, ambos números, y nuestro objeto futuro
                 loop.create_task(setAfter(r, x1, x2, i))
+                #imprimimos el nombre de la operacion que acabamos de realizar y esperamos el resultado de nuestro objeto futuro
                 print(i, await r)
+                #Asignamos nuesro resultado como el valor de la primera x y sumamos uno al contador para no tener que volver a pedir el primer numero
                 x1 = await r
                 contador += 1
 
+#Se manda llamar la funcion principal
 asyncio.run(main())

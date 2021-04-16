b325a6feae2269863b7c43612c4f72b25bd8d53b
G4ll4rd0 <43729882+G4ll4rd0@users.noreply.github.com>
2021-04-16 13:38:05 -0500

    latexToText.py

--- tests/latexToText.py
+++ tests/latexToText.py
@@ -1,4 +1,7 @@
 from pylatexenc.latex2text import LatexNodes2Text
+
+#Se usa la funcion de la libreria para parsearlo
+#? Serta mejor la libreria de https://github.com/JelteF/PyLaTeX ???
 latex = r"x = \frac {-b \pm \sqrt {b^2 - 4ac}}{2a}"
 text = LatexNodes2Text().latex_to_text(latex)
 

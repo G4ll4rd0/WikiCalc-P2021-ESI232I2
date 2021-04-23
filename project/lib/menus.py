dfb4ff243edee5d616bc0fb4e93d898d3aaac4f4
G4ll4rd0 <43729882+G4ll4rd0@users.noreply.github.com>
2021-04-23 14:53:48 -0500

    Update Format, Create Main

--- project/lib/menus.py
+++ project/lib/menus.py
@@ -4,9 +4,4 @@ def mainMenu():
     print("3. Modificar una Función")
     print("4. Salir")
     opc = int(input("Ingrese el numero de opción: "))
-    if opc > 0 or opc < 5:
-        return opc
-    else:
-        print("Esa no es una opción valida, intente de nuevo")
-        print("---------------------------------------------\n")
-        mainMenu()
+    return opc
--- project/main.py
+++ project/main.py
@@ -1,3 +1,18 @@
-import lib.menus
+from lib import menus
 print(" - BIENVENIDO A WIKICALC - ")
-opc = lib.menus.mainMenu()
+opc = menus.mainMenu()
+
+while opc != 4:
+    if opc < 1 or opc > 4:
+        print("Esa no es una opción valida, intente de nuevo")
+        print("---------------------------------------------\n")
+        opc = menus.mainMenu()
+    if opc == 1:
+        print("1. Mostrar todas las Funciones")
+        break
+    if opc == 2:
+        print("2. Agregar una nueva Funcion")
+        break
+    if opc == 3:
+        print("3. Modificar una Función")
+        break
